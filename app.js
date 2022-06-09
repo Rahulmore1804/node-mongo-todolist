f78f0ac04025ccd6dfad2b2d5815ea981d84914a
RahulMore <rahulmore1804@gmail.com>
2022-06-09 22:37:17 +0530

    changed

--- app.js
+++ app.js
@@ -88,10 +88,6 @@ app.post("/delete",function(req,res){
 })
 
 
-
-
-
-
 app.get("/work", function(req,res){
   res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
@@ -104,7 +100,7 @@ let port = process.env.PORT;
 if(port == null || port ==""){
   port = 3000;
 }
-app.listen(port)
+
 app.listen(port, function() {
   console.log("Server started on port 3000");
 });
